5a0c5616bfd3d901254b53da6c2935919197d827
Herman Lundkvist <herlu184@student.liu.se>
2016-12-30 22:51:07 +0100

    first prototype working

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,3 +1,54 @@
+const int ROW_PIN_START = 8;
+const int ROW_PIN_END = 9;//10;
+const int COL_PIN_START = 2;
+const int COL_PIN_END = 3;//7;
+
+//fn levels 0, 1, 2, 3 (3 is only for left half)
+int fn_l = 0;
+int fn_r = 0;
+
+struct key_type{
+  //val == LOW means the key is pressed
+  //val == HIGH means the key is pressed
+  int val;
+  int modifier;
+  int normal_key;
+};
+
+
+struct key_type key_map[12*3*4] =
+{
+  //level 0
+  {HIGH, 0, KEY_Q}, {HIGH, 0, KEY_W}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+    {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+  {HIGH, 0, KEY_A}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+    {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+    {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+  //Left half, level 1
+  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+    {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+    {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+    {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+  //level 2
+  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+    {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+    {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+    {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+  //level 3
+  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+    {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+    {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
+    {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0} 
+};
+
+
 void setup() {
   //start serial connection
   Serial.begin(9600);
@@ -14,75 +65,28 @@ void setup() {
   pinMode(10, OUTPUT_OPENDRAIN);
 }
 
-const int ROW_PIN_START = 8;
-const int ROW_PIN_END = 9;//10;
-const int COL_PIN_START = 2;
-const int COL_PIN_END = 3;//7;
-
-typedef struct{
-  //val == LOW means the key is pressed
-  //val == HIGH means the key is pressed
-  int val;
-  int modifier;
-  int normal_key;
-} key_type;
-
-
-key_type key_map[12*3*4] =
-{
-  //Left half, level 0
-  {HIGH, 0, KEY_Q}, {HIGH, 0, KEY_W}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  {HIGH, 0, KEY_A}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  //Right half, level 0
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  //Left half, level 1
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  //Right half, level 1
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  //Left half, level 2
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  //Right half, level 2
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  //Left half, level 3
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0},
-  {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, {HIGH, 0, 0}, 
-}
-
-//fn levels 0, 1, 2, 3 (3 is only for left half)
-int fn_l = 0;
-int fn_r = 0;
-
 unsigned int key_addr(int x, int y, int l){
   int offs = l*(12*3);
   
-  return offs+12*y+x
+  return offs+12*y+x;
 }
 
-key_type get_key_left(int row_pin, int col_pin, int fn_l){
-  unsigned int addr = key_left_addr(
-                        row_pin - ROW_PIN_START,
+struct key_type get_key_left(int row_pin, int col_pin, int fn_l){
+  unsigned int addr = key_addr(
                         col_pin - COL_PIN_START,
-                        fn_l);  
+                        row_pin - ROW_PIN_START,
+                        fn_l);
   return key_map[addr];
 }
 
+
 void set_key_val_left(int row_pin, int col_pin, int fn_l, int val){
-  unsigned int addr = key_left_addr(
-                        row_pin - ROW_PIN_START,
+  unsigned int addr = key_addr(
                         col_pin - COL_PIN_START,
+                        row_pin - ROW_PIN_START,
                         fn_l);
+  Serial.println("addr");
+  Serial.println(addr);
   key_map[addr].val = val; 
 }
 
@@ -103,11 +107,7 @@ void reset_usb_buff(){
   */
 }
 
-void set_usb(key_type k){
-  Serial.println("SET");
-  Serial.println(k.val);
-  Serial.println(k.normal_key);
-  
+void set_usb(struct key_type k){
   //usb hid keyboard only allows 6 keypresses at once
   if (key_cnt >= 6)
     return;
@@ -147,20 +147,28 @@ void loop() {
   
   int row_pin = ROW_PIN_START;
   for (;row_pin<=ROW_PIN_END;++row_pin){
+    //Serial.println(row_pin);
+    digitalWrite(8, HIGH);
     digitalWrite(9, HIGH);
     digitalWrite(10, HIGH);
-    digitalWrite(11, HIGH);
     digitalWrite(row_pin, LOW);
     //wait for the current row to sink to low
     delayMicroseconds(50);
     
     int col_pin = COL_PIN_START;
     for (;col_pin<=COL_PIN_END;++col_pin){
+      //Serial.println(col_pin);
       int key_val = digitalRead(col_pin);
-      key_type k = get_key_left(row_pin, col_pin, fn_l);
+      struct key_type k = get_key_left(row_pin, col_pin, fn_l);
 
       if (key_val != k.val){
         Serial.println("DIFF");
+        //Serial.println("key_val");
+        //Serial.println(key_val);
+        Serial.println("k.val");
+        Serial.println(k.val);
+        Serial.println("k.normal_key");
+        Serial.println(k.normal_key);
         set_key_val_left(row_pin, col_pin, fn_l, key_val);
         set_usb(k);
       }
