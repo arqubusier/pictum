694fdb076a0e91e51dededf9736c16dc47b9e491
Herman Lundkvist <herlu184@student.liu.se>
2018-04-02 17:47:28 +0200

    Began rewrite

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include "WProgram.h"
 
+#define DEBUG
+
+const int N_INPUTS = 6;
+const int INPUT_PINS[] = {0, 1, 2, 3, 4, 5};
+const int OUTPUT_PINS[] = {6, 7, 8, 9};
+
+void write_matrix(int input_idx){
+    int prev_input_idx = (input_idx - 1 + N_INPUTS)%N_INPUTS;
+    digitalWrite(INPUT_PINS[prev_input_idx], LOW);
+    digitalWrite(INPUT_PINS[input_idx], HIGH);
+    #ifdef DEBUG
+    Serial.print("INPUT PIN ");
+    Serial.print(INPUT_PINS[input_idx]);
+    Serial.println(" HIGH");
+    #endif
+}
+
 extern "C" int main(void)
 {
 #ifdef USING_MAKEFILE
@@ -8,12 +25,21 @@ extern "C" int main(void)
 	// For example:
 
 	pinMode(13, OUTPUT);
+    int incomingByte = 0;
+    int input_idx = 0;
+    int prev_input_idx = 0;
 	while (1) {
-		digitalWriteFast(13, HIGH);
-		delay(200);
-		digitalWriteFast(13, LOW);
-		delay(200);
-        Serial.println("hej");
+        for (input_idx=0; input_idx<N_INPUTS; ++input_idx){
+            write_matrix(input_idx);
+            Serial.println("Press a key on host keyboard to move to the next input");
+            while (Serial.available() <= 0)
+                ;//wait for input
+            incomingByte = Serial.read();
+
+            // say what you got:
+            Serial.print("I received: ");
+            Serial.println(incomingByte, DEC);
+        }
 	}
 
 
